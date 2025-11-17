c2dcb3735cd1cf13374ad12a57e8875ae01c1e2c
munyaradzi <chirovemunyaradzi@gmail.com>
2025-11-17 09:39:37 +0200

    employee

--- havano_zim_payroll/havano_zim_payroll/doctype/havano_employee/havano_employee.py
+++ havano_zim_payroll/havano_zim_payroll/doctype/havano_employee/havano_employee.py
@@ -6,6 +6,7 @@ from . import base_currency,split_currency
 
 class havano_employee(Document):
     def before_save(self):
+        print(self.native_employee_id)
         company = self.company
         
         # Set payslip type from company
@@ -20,18 +21,42 @@ class havano_employee(Document):
 
         # -----------------------------
         # Create ERPNext Employee if not exists
-        # -----------------------------
-        if not frappe.db.exists("Employee", {"employee_number": self.name}):
+                # -----------------------------
+        existing_emp = frappe.db.exists("Employee", self.native_employee_id)
+
+        if existing_emp:
+            # UPDATE EMPLOYEE
+            employee_doc = frappe.get_doc("Employee", existing_emp)
+
+            employee_doc.first_name = self.first_name
+            employee_doc.last_name = self.last_name
+            employee_doc.gender = self.gender
+            employee_doc.status = self.status
+            employee_doc.company = self.company
+            employee_doc.date_of_birth = self.date_of_birth
+            employee_doc.date_of_joining = self.date_of_joining
+
+            employee_doc.save(ignore_permissions=True)
+            frappe.db.commit()
+
+            # frappe.msgprint(f"Updated Employee: {employee_doc.name}")
+
+        else:
+            # CREATE EMPLOYEE
             employee_doc = frappe.get_doc({
                 "doctype": "Employee",
+                "name": self.native_employee_id,
                 "first_name": self.first_name,
-                "last_name":self.last_name,
+                "last_name": self.last_name,
                 "gender": self.gender,
                 "company": self.company,
                 "status": "Active",
                 "date_of_birth": self.date_of_birth,
                 "date_of_joining": self.date_of_joining
             })
+
             employee_doc.insert(ignore_permissions=True)
             frappe.db.commit()
-            print(f"ERPNext Employee created for {self.name}")
+
+            # self.native_employee_id = employee_doc.name
+            frappe.msgprint(f"Created Employee: {employee_doc.name}")
--- havano_zim_payroll/hooks.py
+++ havano_zim_payroll/hooks.py
@@ -247,64 +247,64 @@ app_license = "mit"
 # havano_zim_payroll/hooks.py
 
 # havano_zim_payroll/hooks.py
-fixtures = [
-    # Client Scripts for your module
-    {
-        "doctype": "Client Script",
-        "filters": [
-            ["module", "=", "Havano Zim Payroll"]
-        ]
-    },
-    # Print Formats for your module
-    {
-        "doctype": "Print Format",
-        "filters": [
-            ["module", "=", "Havano Zim Payroll"]
-        ]
-    },
-    # Salary Components
-    {
-        "doctype": "havano_salary_component",
-        "filters": [
-            [
-                "name",
-                "in",
-                [
-                    "Basic Salary",
-                    "PAYEE",
-                    "NSSA",
-                    "NEC",
-                    "Aids Levy",
-                    "Airtime Non Tax",
-                    "Airtime Taxed",
-                    "Overtime Bulk",
-                    "Overtime Short",
-                    "Overtime Double"
-                ]
-            ]
-        ]
-    },
-    # Custom Field for Company
-   {
-    "doctype": "Custom Field",
-    "filters": [["name", "in", ["Company-custom_payslip_type"]]]
-    },
-    # Havano Leave Types
-    {
-        "doctype": "Havano Leave Type",
-        "filters": [
-            [
-                "name",
-                "in",
-                [
-                    "Maternity Leave",
-                    "Annual Leave",
-                    "Sick Leave",
-                    "Bereavement Leave",
-                    "Special Leave",
-                    "Study Leave"
-                ]
-            ]
-        ]
-    }
-]
+# fixtures = [
+#     # Client Scripts for your module
+#     {
+#         "doctype": "Client Script",
+#         "filters": [
+#             ["module", "=", "Havano Zim Payroll"]
+#         ]
+#     },
+#     # Print Formats for your module
+#     {
+#         "doctype": "Print Format",
+#         "filters": [
+#             ["module", "=", "Havano Zim Payroll"]
+#         ]
+#     },
+#     # Salary Components
+#     {
+#         "doctype": "havano_salary_component",
+#         "filters": [
+#             [
+#                 "name",
+#                 "in",
+#                 [
+#                     "Basic Salary",
+#                     "PAYEE",
+#                     "NSSA",
+#                     "NEC",
+#                     "Aids Levy",
+#                     "Airtime Non Tax",
+#                     "Airtime Taxed",
+#                     "Overtime Bulk",
+#                     "Overtime Short",
+#                     "Overtime Double"
+#                 ]
+#             ]
+#         ]
+#     },
+#     # Custom Field for Company
+#    {
+#     "doctype": "Custom Field",
+#     "filters": [["name", "in", ["Company-custom_payslip_type"]]]
+#     },
+#     # Havano Leave Types
+#     {
+#         "doctype": "Havano Leave Type",
+#         "filters": [
+#             [
+#                 "name",
+#                 "in",
+#                 [
+#                     "Maternity Leave",
+#                     "Annual Leave",
+#                     "Sick Leave",
+#                     "Bereavement Leave",
+#                     "Special Leave",
+#                     "Study Leave"
+#                 ]
+#             ]
+#         ]
+#     }
+# ]
