9f57a8b537c4b770cc1105fb2eb8ea34d63c60df
makonia20 <makonia20@gmail.com>
2026-06-10 09:09:40 +0200

    Leave balances and Total Leave Taken additions in Havano Payroll Entry

--- havano_zim_payroll/havano_zim_payroll/doctype/havano_payroll_entry/havano_payroll_entry.js
+++ havano_zim_payroll/havano_zim_payroll/doctype/havano_payroll_entry/havano_payroll_entry.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2025, Havano and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Havano Payroll Entry", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Havano Payroll Entry", {
+	employee: function(frm) {
+		if (frm.doc.employee) {
+			frappe.call({
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Havano Leave Balances",
+					filters: { "employee": frm.doc.employee },
+					fieldname: "leave_balance"
+				},
+				callback: function(r) {
+					if (r.message && r.message.leave_balance !== undefined) {
+						frm.set_value('leave_balances', r.message.leave_balance);
+					} else {
+						frm.set_value('leave_balances', 0);
+					}
+				}
+			});
+		} else {
+			frm.set_value('leave_balances', 0);
+		}
+	}
+});
