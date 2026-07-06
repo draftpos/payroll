5de5c6c3d66943f522581b5dc6b90602ce72bf72
makonia20 <makonia20@gmail.com>
2026-07-06 09:13:14 +0200

    fix: Add Paye Report Store doctype and fix payroll_period field type in report stores
    - Created new paye_report_store doctype with correct PAYE fields
    - Fixed payroll_period field from Link(Payroll Period) to Data in:
      - funeral_report_store
      - leave_report_store
      - medical_aid_report_store
      - nssa_p4_report_store
      - overtime_report_store
      - reports_store_nassa
    - Payroll Period is an ERPNext doctype not available on this site,
      using plain Data field (e.g. July 2025) instead

---
+++ havano_zim_payroll/havano_zim_payroll/doctype/paye_report_store/test_paye_report_store.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Havano and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class TestPayeReportStore(Document):
+	pass
