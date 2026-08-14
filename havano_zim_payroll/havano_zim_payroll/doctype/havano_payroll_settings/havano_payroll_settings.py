ad5fedd58fc5d87d3efa5bf1f7d19b5a6e8952e8
Havano Dev <dev@havano.cloud>
2026-08-14 20:02:37 +0200

    Prevent changing FDS/averaging settings mid-year after payroll processing

--- havano_zim_payroll/havano_zim_payroll/doctype/havano_payroll_settings/havano_payroll_settings.py
+++ havano_zim_payroll/havano_zim_payroll/doctype/havano_payroll_settings/havano_payroll_settings.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2025, Havano and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import getdate, nowdate
+from frappe import _
 
 class HavanoPayrollSettings(Document):
-	pass
+    def validate(self):
+        if not self.is_new():
+            old_doc = self.get_doc_before_save()
+            if old_doc:
+                old_f = old_doc.allow_forecast_fds_method
+                old_a = old_doc.allow_averaging_fds_method
+                new_f = self.allow_forecast_fds_method
+                new_a = self.allow_averaging_fds_method
+
+                if old_f != new_f or old_a != new_a:
+                    current_year = str(getdate(nowdate()).year)
+                    has_payroll = frappe.db.count('Havano Historical PAYE', {'tax_year': current_year})
+                    if has_payroll > 0:
+                        frappe.throw(_('You cannot change FDS or Averaging settings during the year as payroll has already been processed for {0}.').format(current_year))
