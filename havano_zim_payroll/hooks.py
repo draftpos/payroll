0e9a7924a06a09d853d1a9cf5387a7bbd7dab4b3
makonia20 <makonia20@gmail.com>
2026-09-11 16:46:04 +0200

    fix: correct JS file path in hooks.py to include app prefix

--- havano_zim_payroll/hooks.py
+++ havano_zim_payroll/hooks.py
@@ -45,7 +45,7 @@ app_license = "mit"
 # include js in doctype views
 doctype_js = {
 	"Leave Application" : "havano_zim_payroll/public/js/leave_application.js",
-	"Havano Employee" : "public/js/currency_conversions.js"
+	"Havano Employee" : "havano_zim_payroll/public/js/currency_conversions.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
