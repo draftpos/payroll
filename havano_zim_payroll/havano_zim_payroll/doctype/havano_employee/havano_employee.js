e4ed0128277eeefaaec54e3b19d30f53929237d6
makonia20 <makonia20@gmail.com>
2026-09-11 17:27:16 +0200

    refactor: Move dual currency logic to native doctype JS file to ensure loading

--- havano_zim_payroll/havano_zim_payroll/doctype/havano_employee/havano_employee.js
+++ havano_zim_payroll/havano_zim_payroll/doctype/havano_employee/havano_employee.js
@@ -538,3 +538,33 @@ function render_historical_taxable_income(frm) {
 		}
 	});
 }
+
+
+// --- DUAL CURRENCY CONVERSIONS ADDITION ---
+frappe.ui.form.on('havano_employee', {
+    refresh: function(frm) {
+        // Fetch Payroll Settings to see if dual currency is enabled
+        frappe.db.get_single_value('Havano Payroll Settings', 'dual_currency_with_conversions')
+            .then(enabled => {
+                if (enabled == 1 || enabled === "1" || enabled === true) {
+                    frm.set_df_property('employee_category', 'hidden', 0);
+                    // The depends_on condition handles the percentage fields visibility
+                } else {
+                    frm.set_df_property('employee_category', 'hidden', 1);
+                    frm.set_df_property('usd_percentage', 'hidden', 1);
+                    frm.set_df_property('zig_percentage', 'hidden', 1);
+                }
+            });
+    },
+    employee_category: function(frm) {
+        // Just in case depends_on doesn't trigger immediately
+        if (frm.doc.employee_category === 'Both (Zig and USD)') {
+            frm.set_df_property('usd_percentage', 'hidden', 0);
+            frm.set_df_property('zig_percentage', 'hidden', 0);
+        } else {
+            frm.set_df_property('usd_percentage', 'hidden', 1);
+            frm.set_df_property('zig_percentage', 'hidden', 1);
+        }
+    }
+});
+// ------------------------------------------
--- havano_zim_payroll/hooks.py
+++ havano_zim_payroll/hooks.py
@@ -45,7 +45,6 @@ app_license = "mit"
 # include js in doctype views
 doctype_js = {
 	"Leave Application" : "havano_zim_payroll/public/js/leave_application.js",
-	"havano_employee" : "havano_zim_payroll/public/js/currency_conversions.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
